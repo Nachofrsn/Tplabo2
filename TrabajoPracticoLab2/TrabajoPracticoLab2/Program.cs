71ea85d2ba4d8bf71e953610799d33f06b93f1c4
Nachofrsn <icordero@frsn.utn.edu.ar>
2023-10-17 19:59:46 -0300

    casa

--- TrabajoPracticoLab2/TrabajoPracticoLab2/Program.cs
+++ TrabajoPracticoLab2/TrabajoPracticoLab2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
@@ -10,10 +11,10 @@ namespace TpLab2
 {
     public class Program
     {
-        public static string[] menu = new string[] { "1-Generar Despacho.", "2-Salir." };
+        public static string[] menu = new string[] { "1-Generar Despacho.", "2-Listar Despachos.", "3-Salir." };
         static void Main(string[] args)
         {
-            int FijadoColor = 0, LongitudMenu = 2;
+            int FijadoColor = 0, LongitudMenu = 3;
             ConsoleKey key;
             bool OpcionMenuSalir = true;
             do
@@ -57,11 +58,31 @@ namespace TpLab2
                         List<Barco> barcos = new List<Barco>();
                         List<Cliente> clientes = new List<Cliente>();
                         List<Contenedor> contenedores = new List<Contenedor>();
-
                         ObjetosCreados.CreacionObjetos(ref barcos, ref clientes, ref contenedores);
 
+                        string cliente = "";
+                        bool repeat = false;
+                        do
+                        {
+                            try
+                            {
+                                Console.WriteLine("Ingrese un codigo de cliente");
+                                cliente = ManejoArchivos.ChequeoUsuario(Console.ReadLine(), ref clientes);
+                                Console.Clear();
+                                Console.WriteLine("Cliente encontrado!");
+                                repeat = true;
+                            }
+                            catch (ClienteInexistenteException e)
+                            {
+                                Console.WriteLine(e.Message);
+                                repeat = false;
+                            }
+                        } while (repeat == false);
+
+
+                        ManejoArchivos.PrinteoBarcos(ref barcos);
                         Console.WriteLine("Barcos disponibles:\n");
-                        //IMPRIME LOS BARCOS
+
                         for (int i = 0; i < barcos.Count; i++)
                         {
                             if (barcos[i].Disponibilidad == "Disponible")
@@ -74,67 +95,95 @@ namespace TpLab2
                         Console.WriteLine("\nSeleccione un barco");
                         int seleccion = int.Parse(Console.ReadLine());
                         Despacho despacho1 = new Despacho(barcos[seleccion - 1], rnd.Next(9000, 18000));
-
-                        string cliente = "";
-                        bool repeat = false;
-                        List<Cliente> listaClienteDespacho = new List<Cliente>();
-
-                        do
+                        foreach (Cliente aux in clientes)
                         {
-                            try
+                            if (aux.CodigoCliente == cliente)
                             {
-                                Console.WriteLine("Ingrese un cliente");
-                                cliente = Console.ReadLine();
-                                ChequeoUsuario(cliente, clientes, ref listaClienteDespacho);
-
-                                Console.WriteLine("Cliente agregado al despacho, desea seguir ingresando? (ingrese 'si')");
-                                string respuesta = Console.ReadLine();
-                                if (respuesta == "si" || respuesta == "Si" || respuesta == "SI")
+                                despacho1.Cliente = aux;
+                                if (despacho1.Cliente.Exportacion == "contenedor" || despacho1.Cliente.Exportacion == "Contenedor")
                                 {
-                                    repeat = false;
-                                }
-                                else { repeat = true; }
-                            }
-                            catch (ClienteInexistenteException e)
-                            {
-                                Console.WriteLine(e.Message);
-                                repeat = false;
-                            }
-                        } while (repeat == false);
+                                    Console.WriteLine($"Descripcion del producto a exportar: {despacho1.Cliente.DescripcionMercaderia}\n");
+                                    despacho1.Cliente.MostrarContenedores();
 
+                                    Console.WriteLine("Seleccione contenedor a despachar");
+                                    int eleccion = int.Parse(Console.ReadLine());
+
+                                    Console.WriteLine($"\n\n\nResumen del despacho numero: {despacho1.NumeroDespacho}\n\n");
+                                    Console.WriteLine($"Cliente: {despacho1.Cliente.NombreCliente}\nCodigo cliente: {despacho1.Cliente.CodigoCliente}" +
+                                        $"\nMercaderia: {despacho1.Cliente.Mercaderia}\nDescripcion mercaderia: {despacho1.Cliente.DescripcionMercaderia}" +
+                                        $"\n\n");
+                                    despacho1.Cliente.ListarContenedor(eleccion);
+
+                                    Console.WriteLine("\nDesea despachar otro contenedor? (si)"); string eleccion2 = Console.ReadLine();
+                                    if (eleccion2 == "si" || eleccion2 == "SI" || eleccion2 == "Si")
+                                    {
+                                        despacho1.Cliente.MostrarContenedores();
+                                        Console.WriteLine("\nIngrese numero de contenedor a despachar"); eleccion = int.Parse(Console.ReadLine());
+                                        Console.WriteLine($"\n\nCliente: {despacho1.Cliente.NombreCliente}\nCodigo cliente: {despacho1.Cliente.CodigoCliente}" +
+                                        $"\nMercaderia: {despacho1.Cliente.Mercaderia}\nDescripcion mercaderia: {despacho1.Cliente.DescripcionMercaderia}" +
+                                        $"\n\n");
+                                        despacho1.Cliente.ListarContenedor(eleccion);
+                                    }
+                                    else Console.WriteLine("Se cerrara el programa");
+                                }
+                                else if (despacho1.Cliente.Exportacion == "bodega" || despacho1.Cliente.Exportacion == "Bodega")
+                                {
+                                    bool repetir = false;
+                                    while (repetir == false)
+                                    {
+                                        try
+                                        {
+                                            ChequeoToneladas(ref despacho1);
+                                            repetir = true;
+                                        }
+                                        catch (CantidadToneladaBodegaException e)
+                                        {
+                                            Console.WriteLine(e.Message);
+                                            Console.WriteLine($"La capacidad que tiene este barco en bodega es de {despacho1.Barco.TonBodega} toneladas");
+                                            repetir = false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                         OpcionMenuSalir = false;
                         break;
 
                     case 1:
+                        int a = 1;
+                        if (listaDespacho.Count == 0)
+                        {
+                            Console.WriteLine("Aun no se ha realizado un despacho");
+                        }
+                        else
+                        {
+                            foreach (Despacho aux in listaDespacho)
+                            {
+                                Console.WriteLine($"Despacho {1}:\n\n{aux.Barco}{aux.NumeroDespacho}{aux.Cliente.NombreCliente}{aux.Cliente.CodigoCliente}");
+                                a++;
+                            }
+                        }
+                        OpcionMenuSalir = false;
+                        break;
+                    case 2:
                         Console.WriteLine("Eligio salir del programa: cerrando...");
                         OpcionMenuSalir = false;
                         break;
                 }
             } while (OpcionMenuSalir);
-
             Console.ReadKey();
         }
-        public static bool ChequeoUsuario(string cliente, List<Cliente> clientes, ref List<Cliente> listaClienteDespacho)
+
+        public static bool ChequeoToneladas(ref Despacho despacho1)
         {
-            foreach (Cliente aux in clientes)
+            Console.WriteLine("Ingrese cantidad de toneladas a exportar"); int toneladas = int.Parse(Console.ReadLine()); despacho1.Cliente.Toneladas = toneladas;
+            if (despacho1.Barco.TonBodega >= despacho1.Cliente.Toneladas)
             {
-                if (cliente == aux.NombreCliente)
-                {
-                    Console.WriteLine("\nCliente encontrado!\n");
-                    listaClienteDespacho.Add(aux);
-                    if (aux.Exportacion == "Contenedor")
-                    {
-                        Console.WriteLine($"Mercaderia del cliente: {aux.Mercaderia}\n");
-                        aux.MostrarContenedores();
-                        return true;
-                    }
-                    else if (aux.Exportacion == "Bodega")
-                    {
-                        return false;
-                    }
-                }
+                Console.WriteLine($"Descripcion de mercaderia: {despacho1.Cliente.DescripcionMercaderia}\nToneladas a exportar " +
+                $"por el cliente: {despacho1.Cliente.Toneladas}");
+                return true;
             }
-            throw new ClienteInexistenteException();
+            else throw new CantidadToneladaBodegaException();
         }
     }
 }
